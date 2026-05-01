d1fc4342e41116c7e75b00a26d7a1cad14c87c60
Gümüş Hüseynova <144325055+galaksidekikaradelik@users.noreply.github.com>
2026-05-02 00:50:35 +0400

    Add files via upload

---
+++ middleware/roleMiddleware.js
@@ -0,0 +1,25 @@
+const roleMiddleware = (allowedRoles) => {
+    return (req, res, next) => {
+        const roles = Array.isArray(allowedRoles)
+            ? allowedRoles
+            : [allowedRoles];
+
+        // 1. Check if user exists (from authMiddleware)
+        if (!req.user) {
+            return res.status(401).json({
+                message: 'Unauthorized'
+            });
+        }
+
+        // 2. Check role
+        if (!roles.includes(req.user.role)) {
+            return res.status(403).json({
+                message: 'Access denied'
+            });
+        }
+
+        next();
+    };
+};
+
+module.exports = roleMiddleware;
