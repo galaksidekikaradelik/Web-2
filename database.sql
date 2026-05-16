2aa47eab0c9346ef2f76670a9ebdf21bb3fc24c3
Gümüş Hüseynova <144325055+galaksidekikaradelik@users.noreply.github.com>
2026-05-16 13:05:50 +0400

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,25 +1,36 @@
 -- CREATE DATABASE ecommerce_db;
 USE ecommerce_db;
 
+
+
 -- CREATE TABLE categories (
 --     id INT AUTO_INCREMENT PRIMARY KEY,
 --     name VARCHAR(100) NOT NULL
 -- );
 
--- CREATE TABLE products (
---     id INT AUTO_INCREMENT PRIMARY KEY,
---     name VARCHAR(150) NOT NULL,
---     price DECIMAL(10,2) NOT NULL,
---     category_id INT NOT NULL,
---     FOREIGN KEY (category_id) REFERENCES categories(id)
--- );
+CREATE TABLE products (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(150) NOT NULL,
+    price DECIMAL(10,2) NOT NULL,
+    category_id INT NOT NULL,
+    FOREIGN KEY (category_id) REFERENCES categories(id)
+);
+
+ALTER TABLE products
+ADD image_url VARCHAR(255);
 
 
-INSERT INTO categories (name)
-VALUES ('Phones'), ('Laptops'), ('Tablets');
+-- INSERT INTO categories (name)
+-- VALUES ('Phones'), ('Laptops'), ('Tablets');
 
-INSERT INTO products (name, price, category_id)
+INSERT INTO products
+(name, price, category_id, image_url)
 VALUES
-('iPhone 14', 1800, 1),
-('Samsung Galaxy S23', 1500, 1),
-('Lenovo ThinkPad', 2200, 2);
+('iPhone 14', 1800, 1, 'iphone14.png'),
+('Samsung Galaxy S23', 1500, 1, 's23.png'),
+('Lenovo ThinkPad', 2200, 2, 'thinkpad.png');
+
+
+
+
+
