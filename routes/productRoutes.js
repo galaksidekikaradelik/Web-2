6b0ac0753739520cc46dd88059aee66feb30f4a7
Gümüş Hüseynova <144325055+galaksidekikaradelik@users.noreply.github.com>
2026-05-16 11:54:01 +0400

    Update productRoutes.js

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,11 +1,37 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const productController = require('../controllers/productController');
+const productController = require("../controllers/productController");
+
+const authMiddleware = require('../middleware/authMiddleware');
+const roleMiddleware = require('../middleware/roleMiddleware');
+
 
 router.get("/", productController.getAllProducts);
+
+
 router.get("/:id", productController.getProductById);
-router.post("/", productController.createProduct);
-router.put("/:id", productController.updateProduct);
-router.delete("/:id", productController.deleteProduct);
+
+
+router.get("/category/:categoryId", productController.getProductsByCategory);
+
+
+router.post("/", 
+  authMiddleware,
+  roleMiddleware(['admin']),
+  productController.createProduct
+);
+
+router.put("/:id", 
+  authMiddleware,
+  roleMiddleware(['admin']),
+  productController.updateProduct
+);
+
+
+router.delete("/:id", 
+  authMiddleware,
+  roleMiddleware(['admin']),
+  productController.deleteProduct
+);
 
 module.exports = router;
